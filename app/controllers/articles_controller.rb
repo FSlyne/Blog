3f50da1dee1b6c14240c75707b07346adfd96fdd
fslyne <frank.slyne@gmail.com>
2016-05-30 15:24:16 +0000

    initialise repository

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+  def create
+  @article = Article.new(params.require(:article).permit(:title, :text))
+ 
+  @article.save
+  redirect_to @article
+  end
+  def new
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+
+end
